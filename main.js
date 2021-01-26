7b07b6fa86da82285835ee399376bad580499b6f
JLussiez <jlussiez.la-providence.net>
2021-01-26 17:48:09 +0100

    js

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-function ex1(){
+//Fonction de l'exercice 1
+function ex1() {
 
-var largeur;
-var longueur;
-var résultat;
-largeur = prompt("longeur en cm ?");
-largeur = parseFloat(largeur);
-longueur = prompt("largeur en cm ?");
-longueur = parseFloat(longueur);
-résultat =largeur*longueur;
-alert("l'aire du rectangle est de :"+résultat+"cm²");
-alert("Le périmètre vaut :"+2*(Number(longueur)+Number(largeur)));
+   var largeur;
+   var longueur;
+   var résultat;
+   largeur = prompt("longeur en cm ?");
+   largeur = parseFloat(largeur);
+   longueur = prompt("largeur en cm ?");
+   longueur = parseFloat(longueur);
+   résultat = largeur * longueur;
+   alert("l'aire du rectangle est de : " + résultat + " cm²");
+   alert("Le périmètre est de : " + 2 * (Number(longueur) + Number(largeur)) + " cm");
 }
 
+//Fonction de l'exercice 2
+function ex2() {
 
-function ex2(){
+   var rayon = parseFloat(prompt("Entrez le rayon du cercle : "));
+   var périmètre = parseFloat(prompt("Entrez la longueur"));
+   alert("l'aire du cercle est de : " + 3.14 * (Number(rayon) * Number(rayon)) + " cm");
+   alert("Le périmètre du cercle est de : " + Number(périmètre) * 2 * 3.14 + " cm²");
+}
 
-   var rayon=parseFloat(prompt("Entrez le rayon du cercle : "));
-   3.14*rayon*rayon;
+//Fonction de l'exercice 3
+function ex3() {
+   var a = 3;
+   var b = 2;
+   var x = 8;
+   alert("Le résultat de la multiplication entre a et x est : " + x * a);
+   alert("Le résultat de la multiplication entre b et x est : " + x * b);
+   alert("La variable x est : " + x);
+}
+
+//Fonction de l'exercice 4
+function ex4() {
+   let tab = ['-2','1','4'];
+
 }
